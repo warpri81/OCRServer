7be87b2ba2a4cac28f2825f5af3e718f59ef5503
Warren Price <warpri81@gmail.com>
2016-02-05 12:24:58 -0600

    Add command line argument for output file
    Add a second command line argument for the name of the output OCR file.

--- OCRServer.js
+++ OCRServer.js
@@ -2,14 +2,15 @@ var tmp = require('tmp');
 var async = require('async');
 var pdfsearchify = require('./pdfsearchify');
 
-var filename = process.argv[2];
+var infile = process.argv[2]
+  , outfile = process.argv[3];
 var tmpdir = tmp.dirSync({ mode: 0750, prefix: 'OCRServer', keep: true});
 tmp.setGracefulCleanup();
 
 var pages;
 console.log('Output: '+tmpdir.name);
 
-pdfsearchify.searchify(filename, 'results.pdf', tmpdir.name, function(err) {
+pdfsearchify.searchify(infile, outfile, tmpdir.name, function(err) {
     if (err) {
         console.log('ERROR: '+err);
     } else {
