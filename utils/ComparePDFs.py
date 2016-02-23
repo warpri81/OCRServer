5194fe8b16029a6072b414009516dfd08c58d2a3
Warren Price <warpri81@gmail.com>
2016-02-23 10:38:06 -0600

    Add features to ComparePDFs
    Update ComparePDFs so it accepts directories (of same name files). Also add
    file overall comparison, page average, and overall per-page average.

--- utils/ComparePDFs.py
+++ utils/ComparePDFs.py
@@ -1,9 +1,11 @@
-import sys
+import sys, os
 from fuzzywuzzy import fuzz
 from PyPDF2 import PdfFileReader
 
 def comparePDFs(infile1, infile2):
-    scores = []
+    pagescores = []
+    alltext1 = ''
+    alltext2 = ''
     with open(infile1, 'rb') as fp1, open(infile2, 'rb') as fp2:
         pdf1 = PdfFileReader(fp1)
         pdf2 = PdfFileReader(fp2)
@@ -11,15 +13,50 @@ def comparePDFs(infile1, infile2):
         for pagenum in range(pdf1.getNumPages()):
             text1 = pdf1.getPage(pagenum).extractText()
             text2 = pdf2.getPage(pagenum).extractText()
-            scores.append([
+            pagescores.append([
                 fuzz.ratio(text1, text2),
                 fuzz.token_sort_ratio(text1, text2)
             ])
-    return scores
+            alltext1 += text1
+            alltext2 += text2
+    return pagescores, fuzz.ratio(alltext1, alltext2), fuzz.token_sort_ratio(alltext1, alltext2)
+
+def showComparePDFs(infile1, infile2):
+    pagescores, all_ratio_score, all_token_score = comparePDFs(infile1, infile2)
+    infilename1 = os.path.basename(infile1)
+    infilename2 = os.path.basename(infile2)
+    title = infilename1 if infilename1 == infilename2 else infilename1 + ' / ' + infilename2
+    ratio_scores = []
+    token_scores = []
+    print title
+    print '-'*len(title)
+    page = 1
+    print 'Page  Ratio Token'
+    print '===== ===== ====='
+    for ratio_score, token_score in pagescores:
+        print '%5d %5d %5d' % (page, ratio_score, token_score)
+        ratio_scores.append(ratio_score)
+        token_scores.append(token_score)
+        page += 1
+    ratio_avg = sum(ratio_scores) / len(ratio_scores)
+    token_avg = sum(token_scores) / len(token_scores)
+    print '  All %5d %5d' % (all_ratio_score, all_token_score)
+    print '  AVG %5d %5d' % (ratio_avg, token_avg)
+    return ratio_avg, token_avg
 
 if __name__ == '__main__':
-    scores = comparePDFs(sys.argv[-2], sys.argv[-1])
-    page = 1
-    for ratio_score, token_score in scores:
-        print '%5d %5d %5d' % (page, ratio_score, token_score)
-        page += 1
+    path1, path2 = sys.argv[-2:]
+    if os.path.isdir(path1) and os.path.isdir(path2):
+        ratio_avgs = []
+        token_avgs = []
+        path1files = os.listdir(path1)
+        path2files = os.listdir(path2)
+        for f in path1files:
+            if f in path2files:
+                ratio_avg, token_avg = showComparePDFs(os.path.join(path1, f), os.path.join(path2, f))
+                ratio_avgs.append(ratio_avg)
+                token_avgs.append(token_avg)
+                print
+        print 'Overall Average %5d %5d' % (sum(ratio_avgs) / len(ratio_avgs), sum(token_avgs) / len(ratio_avgs))
+    else:
+        showComparePDFs(path1, path2)
