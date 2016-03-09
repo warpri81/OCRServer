47ce79a9e8978faeb5a7c19df9a05379feb1dd97
Warren Price <warpri81@gmail.com>
2016-03-09 11:26:41 -0600

    Update OCRServer events
    Update OCRServer.js events to match the events being emitted and data
    structure of arguments.

--- OCRServer.js
+++ OCRServer.js
@@ -12,18 +12,20 @@ pdfsearchify.on('start', function(o) { console.log('Starting: '+o.infile); });
 pdfsearchify.on('done', function(o) { console.log('Done: '+o.infile+' ('+hrTimeString(o.time)+')'); });
 pdfsearchify.on('compose', function(o) { console.log('Composing: '+o.outfile); });
 pdfsearchify.on('composed', function(o) { console.log('Composed: '+o.outfile+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('startPage', function(o) { console.log('Starting page: '+o.pagenum); });
-pdfsearchify.on('donePage', function(o) { console.log('Done page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('extractPage', function(o) { console.log('Extracting page: '+o.pagenum); });
-pdfsearchify.on('pageExtracted', function(o) { console.log('Extracted page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('deskewPage', function(o) { console.log('Deskewing page: '+o.pagenum); });
-pdfsearchify.on('pageDeskewed', function(o) { console.log('Deskewed page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('preprocessPage', function(o) { console.log('Preprocessing page: '+o.pagenum); });
-pdfsearchify.on('pagePreprocessed', function(o) { console.log('Preprocessed page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('ocrPage', function(o) { console.log('Ocring page: '+o.pagenum); });
-pdfsearchify.on('pageOcred', function(o) { console.log('Ocred page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
-pdfsearchify.on('composePage', function(o) { console.log('Composing page: '+o.pagenum); });
-pdfsearchify.on('pageComposed', function(o) { console.log('Composed page: '+o.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('startPage', function(o) { console.log('Starting page: '+o.processInfo.pagenum); });
+pdfsearchify.on('donePage', function(o) { console.log('Done page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('extractPNM', function(o) { console.log('Extracting page: '+o.processInfo.pagenum); });
+pdfsearchify.on('PNMExtracted', function(o) { console.log('Extracted page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('detectColor', function(o) { console.log('Detecting Color: '+o.processInfo.pagenum); });
+pdfsearchify.on('colorDetected', function(o) { console.log('Detected Color: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('deskewPNM', function(o) { console.log('Deskewing page: '+o.processInfo.pagenum); });
+pdfsearchify.on('PNMDeskewed', function(o) { console.log('Deskewed page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('preprocessPage', function(o) { console.log('Preprocessing page: '+o.processInfo.pagenum); });
+pdfsearchify.on('pagePreprocessed', function(o) { console.log('Preprocessed page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('ocrPage', function(o) { console.log('Ocring page: '+o.processInfo.pagenum); });
+pdfsearchify.on('pageOcred', function(o) { console.log('Ocred page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
+pdfsearchify.on('composePage', function(o) { console.log('Composing page: '+o.processInfo.pagenum); });
+pdfsearchify.on('pageComposed', function(o) { console.log('Composed page: '+o.processInfo.pagenum+' ('+hrTimeString(o.time)+')'); });
 
 
 pdfsearchify(infile, outfile, function(err) {
